5cb631b855a45a4fa4b1bb6e63b2a117475aee32
Prakhar Trivedi <prakhar0706@gmail.com>
2021-03-13 14:43:50 +0800

    Did some firebase stuff

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ client.on('ready', () => {
     console.log('helllo world i am online lmfao')
 })
 
-var hwList = []
+var hwList = [];
 let magaServerID = '780685961079685120'
 let hwChannelID = '800333000671232041'
 let classServerID = '807599800379768862'
@@ -33,14 +33,15 @@ let classServerHwChannelID = '807601775753429002'
 
 // Firebase stuff
 var firebase = require('firebase');
+const { index, falseDependencies } = require('mathjs')
 var app = firebase.initializeApp({
-        apiKey: process.env.FR_API_KEY,
-        authDomain: process.env.FR_AUTH_DOMAIN,
-        projectId: process.env.FR_PRJ_ID,
-        storageBucket: process.env.FR_STORAGE_BUCKET,
-        messagingSenderId: process.env.FR_MSGING_SENDER_ID,
-        appId: process.env.FR_APP_ID,
-        measurementId: process.env.FR_MEASUREMENT_ID
+    apiKey: process.env.FR_API_KEY,
+    authDomain: process.env.FR_AUTH_DOMAIN,
+    projectId: process.env.FR_PRJ_ID,
+    storageBucket: process.env.FR_STORAGE_BUCKET,
+    messagingSenderId: process.env.FR_MSGING_SENDER_ID,
+    appId: process.env.FR_APP_ID,
+    measurementId: process.env.FR_MEASUREMENT_ID
 })
 // functions
 var database = firebase.database()
@@ -51,14 +52,15 @@ async function addItem(subject, content) {
     } else {
         newIndex = 1
     }
-    await database.ref("items/" + `${(newIndex)}` + "/" + subject).set({
+    await database.ref("items/" + `${(newIndex)}`).set({
+        subject: subject,
         content: content
     })
 }
 function removeItem(index) {
     database.ref("items/" + (index)).remove()
 }
- 
+
 // Realtime Listeners
 // Data Change
 var itemsRef = database.ref("items/");
@@ -68,6 +70,13 @@ itemsRef.on('value', (snapshot) => {
     console.log(hwList)
 })
 
+function dataToContent(object) {
+    if (!object) return console.log("No object provided.")
+    let subject = object.subject
+    let content = object.content
+    return { subject, content }
+}
+
 // end of firebase stuff
 
 client.on('message', message => {
@@ -75,6 +84,9 @@ client.on('message', message => {
     if (!message.content.startsWith(Prefix)) return
     if (!message.guild) return message.reply('Please use this bot in a guild!')
     let args = message.content.substring(Prefix.length).split(' ');
+
+    // firebase testing code
+
     if (args[0] == "he") {
         addItem("he", args[1])
     } else if (args[0] == 'ra') {
@@ -82,111 +94,134 @@ client.on('message', message => {
     } else if (args[0] == 'r') {
         removeItem(args[1])
     } else if (args[0] == 'g') {
-        console.log(hwList[parseInt(args[1])].he.content)
+        console.log(hwList[parseInt(args[1])].subject)
+        console.log(hwList[parseInt(args[1])].content)
     }
-    // if (args[0] == "ping") {
-    //     message.reply('pong!')
-    // } else if (args[0] == "kanye" || args[0] == 'k') {
-    //     message.reply('Here\'s a pic of Kanye! :)')
-    //     message.channel.send('https://upload.wikimedia.org/wikipedia/commons/0/0f/Kanye_West_at_the_2009_Tribeca_Film_Festival-2_%28cropped%29.jpg')
-    // } else if (args[0] == "hi") {
-    //     message.channel.send('Hey there! I am a school bot designed and built for this specific server by Prakhar Trivedi.')
-    //     message.channel.send('https://prakhar896.github.io')
-    // } else if (args[0] == "hw") {
-    //     if (args[1] == "list") {
-    //         const hwEmbed = new Discord.MessageEmbed()
-    //             .setTitle('List of Homework items');
-    //         for (hwItem of hwList) {
-    //             var indexOfColon = hwItem.indexOf(":")
-    //             var subtitleOfHw = hwItem.substring(indexOfColon + 1)
-    //             var title = hwItem.substring(0, indexOfColon)
-    //             if (!title || !subtitleOfHw) {
-    //                 hwEmbed.addField('Fatal Error:', 'Could not find title or description of homework item. Please delete this item and try again.')
-    //             } else {
-    //                 hwEmbed.addField(title, subtitleOfHw)
-    //             }
-    //         }
-    //         message.channel.send(hwEmbed)
-    //     } else if (args[1] == "add") {
-    //         let newItem = message.content.slice(Prefix.length - 1 + 8)
-    //         if (!newItem) return message.reply('Please give the new homework item in the correct format. Type !hw help for more information')
-    //         if (!(newItem.indexOf(':') > -1)) return message.reply('Please use the correct format to add a homework entry.')
-    //         hwList.push(newItem)
-    //         message.reply('Added new item successfully!')
-    //         message.channel.send('Showing homework list with new changes...')
 
-    //         const hwEmbed = new Discord.MessageEmbed()
-    //             .setTitle('List of Homework items');
-    //         for (hwItem of hwList) {
-    //             var indexOfColon = hwItem.indexOf(":")
-    //             var subtitleOfHw = hwItem.substring(indexOfColon + 1)
-    //             var title = hwItem.substring(0, indexOfColon)
-    //             if (!title || !subtitleOfHw) {
-    //                 hwEmbed.addField('Fatal Error:', 'Could not find title or description of homework item. Please delete this item and try again.')
-    //             } else {
-    //                 hwEmbed.addField(title, subtitleOfHw)
-    //             }
-    //         }
-    //         message.channel.send(hwEmbed)
+    //end of firebase testing code
+    /*
+    need to be reconfigured to be like
+    1 {
+        subject: "he"
+        content: "mama"
+    }
+    */
+    if (args[0] == "ping") {
+        message.reply('pong!')
+    } else if (args[0] == "kanye" || args[0] == 'k') {
+        message.reply('Here\'s a pic of Kanye! :)')
+        message.channel.send('https://upload.wikimedia.org/wikipedia/commons/0/0f/Kanye_West_at_the_2009_Tribeca_Film_Festival-2_%28cropped%29.jpg')
+    } else if (args[0] == "hi") {
+        message.channel.send('Hey there! I am a school bot designed and built for school server by Prakhar Trivedi.')
+        message.channel.send('https://prakhar896.github.io')
+    } else if (args[0] == "hw") {
+        if (args[1] == "list") {
+            const hwEmbed = new Discord.MessageEmbed()
+                .setTitle('List of Homework items');
+            if (!hwList) return message.reply('There is no homework!!! Celebrate! :partying_face:')
+            for (hwItem of hwList) {
+                if (hwItem) {
+                    console.log(hwItem)
+                    var subtitleOfHw = hwItem.content
+                    var title = hwItem.subject
+                    if (!title || !subtitleOfHw) {
+                        hwEmbed.addField('Fatal Error:', 'Could not find title or description of homework item. Please delete this item and try again.')
+                    } else {
+                        hwEmbed.addField(title, subtitleOfHw)
+                    }
+                }
+            }
+            message.channel.send(hwEmbed)
+        } else if (args[1] == "add") {
+            let newItem = message.content.slice(Prefix.length - 1 + 8)
+            if (!newItem) return message.reply('Please give the new homework item in the correct format. Type !hw help for more information')
+            if (!(newItem.indexOf(':') > -1)) return message.reply('Please use the correct format to add a homework entry.')
+            newItem = newItem.split(':')
+            let subject = newItem[0]
+            let content = newItem.slice(1).join(' ')
+            addItem(subject, content)
+            message.reply('Added new item successfully!')
+            message.channel.send('Showing homework list with new changes...')
 
-    //     } else if (args[1] == "help") {
-    //         let hwHelpEmbed = new Discord.MessageEmbed()
-    //             .setTitle('Homework List Command Help')
-    //             .addField('This command is made to help you track all the homework you get from school easily with this bot.', 'Each homework item contains a title and a details part.')
-    //             .addField('Getting homework list command format:', '!hw list')
-    //             .addField('Adding a homework item command format:', '!hw add <title>:<details or description>')
-    //             .addField('Example of homework adding command:', '!hw add Emaths:TB Page 31, Practise Now 3')
-    //             .addField('Deleting a homework item command format: ', '!hw delete <count of item>')
-    //             .addField('The homework list counting starts with 1 being the first item in the list', 'Example if I wanted to delete second item in list: !hw delete 2')
-    //             .addField('Deleting all Homework Items', 'If you want to delete the entire list, type !hw wipe')
-    //         message.channel.send(hwHelpEmbed)
-    //     } else if (args[1] == 'delete') {
-    //         let countOfItemToBeDeleted = args[2]
-    //         if (!countOfItemToBeDeleted) return message.reply('Please give the number of the homework item to delete. Type !hw help for more information.')
-    //         if (countOfItemToBeDeleted - 1 > hwList.length) return message.reply('Please give a valid homework item count.')
-    //         if (countOfItemToBeDeleted - 1 > -1) {
-    //             hwList.splice(countOfItemToBeDeleted - 1, 1);
-    //         } else {
-    //             message.reply('Please give a valid homework item count.')
-    //             return
-    //         }
-    //         message.reply('Deleted homework item successfully!')
-    //         message.channel.send('Showing homework list with new changes...')
+            const hwEmbed = new Discord.MessageEmbed()
+                .setTitle('List of Homework items');
+            if (!hwList) return message.reply('There is no homework!!! Celebrate! :partying_face:')
+            for (hwItem of hwList) {
+                if (hwItem) {
+                    console.log(hwItem)
+                    var subtitleOfHw = hwItem.content
+                    var title = hwItem.subject
+                    if (!title || !subtitleOfHw) {
+                        hwEmbed.addField('Fatal Error:', 'Could not find title or description of homework item. Please delete this item and try again.')
+                    } else {
+                        hwEmbed.addField(title, subtitleOfHw)
+                    }
+                }
+            }
+            message.channel.send(hwEmbed)
 
-    //         const hwEmbed = new Discord.MessageEmbed()
-    //             .setTitle('List of Homework items');
-    //         for (hwItem of hwList) {
-    //             var indexOfColon = hwItem.indexOf(":")
-    //             var subtitleOfHw = hwItem.substring(indexOfColon + 1)
-    //             var title = hwItem.substring(0, indexOfColon)
-    //             if (!title || !subtitleOfHw) {
-    //                 hwEmbed.addField('Fatal Error:', 'Could not find title or description of homework item. Please delete this item and try again.')
-    //             } else {
-    //                 hwEmbed.addField(title, subtitleOfHw)
-    //             }
-    //         }
-    //         message.channel.send(hwEmbed)
-            
-    //     } else if (args[1] == 'wipe') {
-    //         hwList = []
-    //         message.channel.send('Wiped the Homework List successfully!')
-    //         message.channel.send('Showing homework list with new changes...')
+        } else if (args[1] == "help") {
+            let hwHelpEmbed = new Discord.MessageEmbed()
+                .setTitle('Homework List Command Help')
+                .addField('This command is made to help you track all the homework you get from school easily with this bot.', 'Each homework item contains a title and a details part.')
+                .addField('Getting homework list command format:', '!hw list')
+                .addField('Adding a homework item command format:', '!hw add <title>:<details or description>')
+                .addField('Example of homework adding command:', '!hw add Emaths:TB Page 31, Practise Now 3')
+                .addField('Deleting a homework item command format: ', '!hw delete <count of item>')
+                .addField('The homework list counting starts with 1 being the first item in the list', 'Example if I wanted to delete second item in list: !hw delete 2')
+                .addField('Deleting all Homework Items', 'If you want to delete the entire list, type !hw wipe')
+            message.channel.send(hwHelpEmbed)
+        } else if (args[1] == 'delete') {
+            let countOfItemToBeDeleted = args[2]
+            if (!countOfItemToBeDeleted) return message.reply('Please give the number of the homework item to delete. Type !hw help for more information.')
+            if (countOfItemToBeDeleted - 1 > (hwList.length - 1)) return message.reply('Please give a valid homework item count.')
+            if (countOfItemToBeDeleted - 1 > -1) {
+                let found = false
+                let currentIndex = parseInt(countOfItemToBeDeleted)
+                console.log(currentIndex)
+                console.log(currentIndex)
+                database.ref("items/" + String(currentIndex)).remove()
+            } else {
+                message.reply('Please give a valid homework item count.')
+                return
+            }
+            message.reply('Deleted homework item successfully!')
+            message.channel.send('Showing homework list with new changes...')
 
-    //         const hwEmbed = new Discord.MessageEmbed()
-    //             .setTitle('List of Homework items');
-    //         for (hwItem of hwList) {
-    //             var indexOfColon = hwItem.indexOf(":")
-    //             var subtitleOfHw = hwItem.substring(indexOfColon + 1)
-    //             var title = hwItem.substring(0, indexOfColon)
-    //             if (!title || !subtitleOfHw) {
-    //                 hwEmbed.addField('Fatal Error:', 'Could not find title or description of homework item. Please delete this item and try again.')
-    //             } else {
-    //                 hwEmbed.addField(title, subtitleOfHw)
-    //             }
-    //         }
-    //         message.channel.send(hwEmbed)
-    //     }
-    // }
+            const hwEmbed = new Discord.MessageEmbed()
+                .setTitle('List of Homework items');
+            for (hwItem of hwList) {
+                var indexOfColon = hwItem.indexOf(":")
+                var subtitleOfHw = hwItem.substring(indexOfColon + 1)
+                var title = hwItem.substring(0, indexOfColon)
+                if (!title || !subtitleOfHw) {
+                    hwEmbed.addField('Fatal Error:', 'Could not find title or description of homework item. Please delete this item and try again.')
+                } else {
+                    hwEmbed.addField(title, subtitleOfHw)
+                }
+            }
+            message.channel.send(hwEmbed)
+
+        } else if (args[1] == 'wipe') {
+            hwList = []
+            message.channel.send('Wiped the Homework List successfully!')
+            message.channel.send('Showing homework list with new changes...')
+
+            const hwEmbed = new Discord.MessageEmbed()
+                .setTitle('List of Homework items');
+            for (hwItem of hwList) {
+                var indexOfColon = hwItem.indexOf(":")
+                var subtitleOfHw = hwItem.substring(indexOfColon + 1)
+                var title = hwItem.substring(0, indexOfColon)
+                if (!title || !subtitleOfHw) {
+                    hwEmbed.addField('Fatal Error:', 'Could not find title or description of homework item. Please delete this item and try again.')
+                } else {
+                    hwEmbed.addField(title, subtitleOfHw)
+                }
+            }
+            message.channel.send(hwEmbed)
+        }
+    }
 })
 
 client.on('messageDelete', deletedMessage => {
@@ -194,13 +229,3 @@ client.on('messageDelete', deletedMessage => {
 })
 
 client.login(process.env.DISCORD_BETA_TOKEN)
-
-
-//database structure
-
-// {
-//     "items": {
-//         "item1": "Content",
-//         "item2": "content"
-//     }
-// }
